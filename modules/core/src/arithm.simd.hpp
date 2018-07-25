986e9d1a58b99f15d09bb4e8ac8888e09dfb8f2b
Sayed Adel <seiko@imavr.com>
2018-07-25 22:00:37 +0200

    initialize arithm dispatcher

---
+++ modules/core/src/arithm.simd.hpp
@@ -0,0 +1,13 @@
+// This file is part of OpenCV project.
+// It is subject to the license terms in the LICENSE file found in the top-level directory
+// of this distribution and at http://opencv.org/license.html
+
+#include "opencv2/core/hal/intrin.hpp"
+
+namespace cv { namespace hal {
+
+CV_CPU_OPTIMIZATION_NAMESPACE_BEGIN
+
+CV_CPU_OPTIMIZATION_NAMESPACE_END
+
+}} // cv::hal::
